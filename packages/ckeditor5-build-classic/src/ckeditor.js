416b47e375b8f03d622876deec866bff1139703e
Roman Gonzalez <solay2516@gmail.com>
2021-02-05 11:51:41 -0600

    change default lang to en

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -102,5 +102,5 @@ ClassicEditor.defaultConfig = {
 		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: "es",
+	language: "en",
 };
