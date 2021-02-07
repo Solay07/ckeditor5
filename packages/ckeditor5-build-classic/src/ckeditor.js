206c395dd63bb9cbfd51b848f887ab161d2b5cf5
Roman Gonzalez <solay2516@gmail.com>
2021-02-06 21:58:47 -0600

    feat: add support for presentational image styles

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -88,12 +88,31 @@ ClassicEditor.defaultConfig = {
 		],
 	},
 	image: {
+		styles: ["alignLeft", "alignCenter", "alignRight"],
+		// Configure the available image resize options.
+		resizeOptions: [
+			{
+				name: "imageResize:original",
+				label: "Original",
+				value: null,
+			},
+			{
+				name: "imageResize:50",
+				label: "50%",
+				value: "50",
+			},
+			{
+				name: "imageResize:75",
+				label: "75%",
+				value: "75",
+			},
+		],
 		toolbar: [
-			"imageStyle:full",
-			"imagestyle:side",
 			"imageStyle:alignLeft",
 			"imageStyle:alignCenter",
 			"imageStyle:alignRight",
+			"|",
+			"imageResize",
 			"|",
 			"imageTextAlternative",
 		],
